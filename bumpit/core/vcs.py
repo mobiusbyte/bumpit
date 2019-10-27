a848f80a0bd26e9fb0f6861f3b7347e55a3d69cf
Jill San Luis <jill.devs@gmail.com>
2019-10-27 09:58:07 -0400

    Dry run tagging. See #6 and #8

--- bumpit/core/vcs.py
+++ bumpit/core/vcs.py
@@ -2,16 +2,39 @@ import os
 
 
 class Git:
-    def __init__(self, dry_run, logger, command_executor=None):
+    def __init__(self, dry_run, tag, tag_format, logger, command_executor=None):
         self._dry_run = dry_run
+        self._tag = tag
+        self._tag_format = self._parse_tag_format(tag_format)
         self._logger = logger
         self._command_executor = command_executor or os.system
 
     def update(self, current_version, bumped_version):
+        self._git_commit(current_version, bumped_version)
+        self._git_tag(current_version, bumped_version)
+
+    def _git_commit(self, current_version, bumped_version):
         self._execute_command("git add .")
         self._execute_command(
-            f"git commit -m 'Bumped version from {current_version} → {bumped_version}.'"
+            f"git commit -m '{self._bump_message(current_version, bumped_version)}'"
         )
+
+    def _git_tag(self, current_version, bumped_version):
+        if not self._tag:
+            self._logger.info("Skipped tagging...")
+            return
+
+        tag_command_format = (
+            f"git tag "
+            f"-a '{self._tag_format}' "
+            f"-m '{self._bump_message(current_version, bumped_version)}'"
+        )
+        tag_command = tag_command_format.format(version=bumped_version)
+
+        self._execute_command(tag_command)
+
+    def _bump_message(self, current_version, bumped_version):
+        return f"Bumped version from {current_version} → {bumped_version}."
 
     def _execute_command(self, command):
         if self._dry_run:
@@ -21,3 +44,12 @@ class Git:
                 raise Exception(f"Failed to execute {command}")
 
             self._logger.info(f"[OK] {command}")
+
+    @staticmethod
+    def _parse_tag_format(tag_format):
+        if "{version}" not in tag_format:
+            raise ValueError(
+                f"Invalid tag_format '{tag_format}'. "
+                f"Value must include the string `{{version}}`."
+            )
+        return tag_format
--- tests/unit/core/test_vcs.py
+++ tests/unit/core/test_vcs.py
@@ -21,12 +21,15 @@ class TestGit:
     def setup(self):
         self._logger_spy = LoggerSpy()
         self._command_executor = CommandExecutorSpy()
+        self._tag_format = "v{version}"
         self._current_version = "0.0.0"
         self._bumped_version = "1.0.0"
 
-    def test_update_dry_run(self):
+    def test_update_dry_run_tagging_disabled(self):
         git = Git(
             dry_run=True,
+            tag=False,
+            tag_format=self._tag_format,
             logger=self._logger_spy,
             command_executor=self._command_executor,
         )
@@ -35,12 +38,15 @@ class TestGit:
         assert self._command_executor.call_count == 0
         assert [
             "[DRY-RUN] Ran `git add .`",
-            ("[DRY-RUN] " "Ran `git commit -m 'Bumped version from 0.0.0 → 1.0.0.'`"),
+            "[DRY-RUN] Ran `git commit -m 'Bumped version from 0.0.0 → 1.0.0.'`",
+            "Skipped tagging...",
         ] == self._logger_spy.messages
 
-    def test_update(self):
+    def test_update_tagging_disabled(self):
         git = Git(
             dry_run=False,
+            tag=False,
+            tag_format=self._tag_format,
             logger=self._logger_spy,
             command_executor=self._command_executor,
         )
@@ -53,13 +59,48 @@ class TestGit:
         assert [
             "[OK] git add .",
             "[OK] git commit -m 'Bumped version from 0.0.0 → 1.0.0.'",
+            "Skipped tagging...",
         ] == self._logger_spy.messages
+
+    def test_update_dry_run_tagging_enabled(self):
+        git = Git(
+            dry_run=True,
+            tag=True,
+            tag_format=self._tag_format,
+            logger=self._logger_spy,
+            command_executor=self._command_executor,
+        )
+        git.update(self._current_version, self._bumped_version)
+
+        assert self._command_executor.call_count == 0
+        assert [
+            "[DRY-RUN] Ran `git add .`",
+            "[DRY-RUN] Ran `git commit -m 'Bumped version from 0.0.0 → 1.0.0.'`",
+            (
+                "[DRY-RUN] "
+                "Ran `git tag -a 'v1.0.0' -m 'Bumped version from 0.0.0 → 1.0.0.'`"
+            ),
+        ] == self._logger_spy.messages
+
+    def test_invalid_tag_format(self):
+        with pytest.raises(ValueError):
+            Git(
+                dry_run=True,
+                tag=True,
+                tag_format="invalid_format",
+                logger=self._logger_spy,
+                command_executor=self._command_executor,
+            )
 
     def test_update_non_zero_exist_code(self):
         command_executor = CommandExecutorSpy(return_code=1)
 
         git = Git(
-            dry_run=False, logger=self._logger_spy, command_executor=command_executor
+            dry_run=False,
+            tag=False,
+            tag_format=self._tag_format,
+            logger=self._logger_spy,
+            command_executor=command_executor,
         )
         with pytest.raises(Exception):
             git.update(self._current_version, self._bumped_version)
