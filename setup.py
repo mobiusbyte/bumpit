ad5624383dcafe2e95ebedc19f3072c7f5e12b8c
Jill San Luis <jill.devs@gmail.com>
2019-10-30 21:48:04 -0400

    Bumped version from 0.0.4 → 0.1.0.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "rt", encoding="utf8") as f:
 
 setup(
     name="bumpit",
-    version="0.0.4",
+    version="0.1.0",
     description="A small command line tool to bump tracked versions in your repository.",
     long_description=readme,
     long_description_content_type="text/markdown",
