57fda45d5a82047cffe64dd674e3b164bab06ce3
Jill San Luis <jill.devs@gmail.com>
2019-11-01 23:53:28 -0400

    Rearranging the guts of the runner to make space for new semver strategy. See #16

--- bumpit/core/bumpit.py
+++ bumpit/core/bumpit.py
@@ -7,21 +7,36 @@ from bumpit.core.version_strategy import new_version
 
 
 def run(config, logger, dry_run):
-    configuration = Configuration.parse(config)
-    executor = BumpIt(folder=os.getcwd(), logger=logger, dry_run=dry_run)
-    vcs = Git(
-        dry_run=dry_run,
-        tag=configuration.tag,
-        tag_format=configuration.tag_format,
-        auto_remote_push=configuration.auto_remote_push,
-        logger=logger,
-    )
+    runner = Runner(config, dry_run, logger)
 
-    current_version = configuration.current_version
-    bumped_version = new_version(configuration.strategy, current_version)
-    executor.bump(
-        current_version=current_version,
-        bumped_version=bumped_version,
-        files=configuration.tracked_files + [config],
-    )
-    vcs.update(current_version=current_version, bumped_version=bumped_version)
+    configuration = Configuration.parse(config)
+
+    bumped_version = new_version(configuration.strategy, configuration.current_version)
+    runner.execute(bumped_version)
+
+
+class Runner:
+    def __init__(self, config, dry_run, logger):
+        configuration = Configuration.parse(config)
+
+        self._executor = BumpIt(folder=os.getcwd(), logger=logger, dry_run=dry_run)
+        self._vcs = Git(
+            dry_run=dry_run,
+            tag=configuration.tag,
+            tag_format=configuration.tag_format,
+            auto_remote_push=configuration.auto_remote_push,
+            logger=logger,
+        )
+        self._current_version = configuration.current_version
+        self._tracked_files = configuration.tracked_files + [config]
+
+    def execute(self, new_version):
+        self._executor.bump(
+            current_version=self._current_version,
+            bumped_version=new_version,
+            files=self._tracked_files,
+        )
+
+        self._vcs.update(
+            current_version=self._current_version, bumped_version=new_version
+        )
