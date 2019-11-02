19e73d0b3c68969ec697dbe418a68e1d5393b4cc
Jill San Luis <jill.devs@gmail.com>
2019-11-01 22:12:45 -0400

    Implement more complete SemVer parser. See #16

---
+++ bumpit/core/versions/semver.py
@@ -0,0 +1,30 @@
+import re
+from dataclasses import dataclass
+
+
+class InvalidVersion(Exception):
+    pass
+
+
+@dataclass
+class SemVer:
+    major: int
+    minor: int
+    patch: int
+    pre_release: str
+    build_metadata: str
+
+    @staticmethod
+    def parse(version):
+        semver_pattern = "^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$"  # noqa
+        match = re.search(semver_pattern, version)
+        if not match:
+            raise InvalidVersion(f"Invalid semantic version format '{version}")
+
+        return SemVer(
+            major=int(match.group(1)),
+            minor=int(match.group(2)),
+            patch=int(match.group(3)),
+            pre_release=match.group(4) or "",
+            build_metadata=match.group(5) or "",
+        )
