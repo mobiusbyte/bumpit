df7558867b146a7f25c0ecd6104baa03402fa41b
Jill San Luis <jill.devs@gmail.com>
2019-11-02 11:44:07 -0400

    More clean up. Pushing semver logic as far down in server module. See #16

--- tests/unit/core/test_versions.py
+++ tests/unit/core/test_versions.py
@@ -2,29 +2,15 @@ import pytest
 from bumpit.core.versions import next_version, SEMVER_STRATEGY
 
 
-@pytest.mark.parametrize(
-    "part, force_value, expected_next_version",
-    [
-        pytest.param("major", None, "2.0.0"),
-        pytest.param("minor", None, "1.3.0"),
-        pytest.param("patch", None, "1.2.4"),
-        pytest.param("pre_release", "beta", "1.2.3-beta"),
-        pytest.param("pre_release", "", "1.2.3"),
-        pytest.param(
-            "build_metadata", "exp.sha.5114f85", "1.2.3-alpha+exp.sha.5114f85"
-        ),
-        pytest.param("major", "9", "9.0.0"),
-        pytest.param("minor", "9", "1.9.0"),
-        pytest.param("patch", "9", "1.2.9"),
-    ],
-)
-def test_next_version_semver(part, force_value, expected_next_version):
-    assert expected_next_version == next_version(
+def test_next_version_semver():
+    expected_next_version = "2.0.0"
+    actual_next_version = next_version(
         strategy=SEMVER_STRATEGY,
         current_version="1.2.3-alpha+exp.sha.1234f56",
-        part=part,
-        force_value=force_value,
+        part="major",
+        force_value=None,
     )
+    assert expected_next_version == actual_next_version
 
 
 def test_next_version_unsupported_strategy():
