2d8ca0d9cd2c77b220d718581598f64f01b8286f
Jill San Luis <jill.devs@gmail.com>
2019-11-02 13:20:36 -0400

    Adding validator for cal ver tokens. See #14

--- bumpit/core/versions/calver.py
+++ bumpit/core/versions/calver.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 CALVER_STRATEGY = "calver"
 
 CALVER_AUTO = "auto"
@@ -7,29 +9,53 @@ def next_calendar_version(current_version, part, force_value, version_format):
     return "2019.11.03"
 
 
+@dataclass
+class TokenMatcherSpec:
+    token: str
+    regex_pattern: str
+
+
 class VersionParser:
     def __init__(self, version_format):
         self._init_matcher(version_format)
 
     def _init_matcher(self, version_format):
 
+        # (0|[1-9]\\d*)
         mutex_tokens = [
-            ["YYYY", "YY", "0Y"],
-            ["MM", "0M"],
-            ["DD", "0D"],
-            ["MINOR"],
-            ["MICRO"],
-            ["MODIFIER"],
+            [
+                TokenMatcherSpec(token="YYYY", regex_pattern="(\\d{4})"),
+                TokenMatcherSpec(token="YY", regex_pattern="(\\d{1,2})"),
+                TokenMatcherSpec(token="0Y", regex_pattern="(\\d{2})"),
+            ],
+            [
+                TokenMatcherSpec(token="MM", regex_pattern="(\\d{1,2})"),
+                TokenMatcherSpec(token="0M", regex_pattern="(\\d{2})"),
+            ],
+            [
+                TokenMatcherSpec(token="DD", regex_pattern="(\\d{1,2})"),
+                TokenMatcherSpec(token="0D", regex_pattern="(\\d{2})"),
+            ],
+            [TokenMatcherSpec(token="MINOR", regex_pattern="(0|[1-9]\\d*))")],
+            [TokenMatcherSpec(token="MICRO", regex_pattern="(0|[1-9]\\d*))")],
+            [
+                TokenMatcherSpec(
+                    token="MODIFIER", regex_pattern="(?:\\.[0-9a-zA-Z-]+)*))"
+                )
+            ],
         ]
 
         remaining_version_format = version_format
         for mutex_tokens_set in mutex_tokens:
-            for index, x_token in enumerate(mutex_tokens_set):
-                if x_token in remaining_version_format:
-                    remaining_version_format = version_format.replace(x_token, "", 1)
+            for index, x_spec in enumerate(mutex_tokens_set):
+                if x_spec.token in remaining_version_format:
+                    remaining_version_format = version_format.replace(
+                        x_spec.token, "", 1
+                    )
 
-                    for y_token in mutex_tokens_set[index:]:
-                        if y_token in remaining_version_format:
+                    for y_spec in mutex_tokens_set[index:]:
+                        if y_spec.token in remaining_version_format:
                             raise ValueError(
-                                f"Cannot have {y_token} when there is already {x_token}"
+                                f"Cannot have '{y_spec.token}'. "
+                                f"The '{x_spec.token}' already exists."
                             )
