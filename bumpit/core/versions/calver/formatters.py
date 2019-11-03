6b8228d3350a830f87764ee3010fb99f0d711c28
Jill San Luis <jill.devs@gmail.com>
2019-11-03 14:33:40 -0500

    Cleaning up calver.formatters. See #27

--- bumpit/core/versions/calver/formatters.py
+++ bumpit/core/versions/calver/formatters.py
@@ -6,20 +6,23 @@ def build_formatter(regex_pattern, token_specs, match):
 
     for i, token_spec in enumerate(token_specs):
         group = match.group(i + 1)
-
-        if token_spec.part_type == YEAR_PART:
-            partial = {
-                4: "{calendar_date:%Y}",
-                2: "{calendar_date:%y}",
-                1: "{calendar_short_year}",
-            }[len(group)]
-        elif token_spec.part_type == MONTH_PART:
-            partial = {2: "{calendar_date:%m}", 1: "{calendar_date.month}"}[len(group)]
-        elif token_spec.part_type == DAY_PART:
-            partial = {2: "{calendar_date:%d}", 1: "{calendar_date.day}"}[len(group)]
-        else:
-            partial = f"{{{token_spec.part_type}}}"
+        partial = _partial_formatter(token_spec, group)
 
         formatter = formatter.replace(token_spec.regex_pattern, partial, 1)
 
     return formatter
+
+
+def _partial_formatter(token_spec, group):
+    if token_spec.part_type == YEAR_PART:
+        return {
+            4: "{calendar_date:%Y}",
+            2: "{calendar_date:%y}",
+            1: "{calendar_short_year}",
+        }[len(group)]
+    elif token_spec.part_type == MONTH_PART:
+        return {2: "{calendar_date:%m}", 1: "{calendar_date.month}"}[len(group)]
+    elif token_spec.part_type == DAY_PART:
+        return {2: "{calendar_date:%d}", 1: "{calendar_date.day}"}[len(group)]
+    else:
+        return f"{{{token_spec.part_type}}}"
